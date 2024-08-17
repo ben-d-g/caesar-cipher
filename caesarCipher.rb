98c611f569b261425916475ac7b230d3bf920e5c
Ben <ben.d.godfrey2002@gmail.com>
2024-08-17 15:26:18 +0100

    Tested caesarCipher

--- caesarCipher.rb
+++ caesarCipher.rb
@@ -18,3 +18,24 @@ def encrypt(string, offset)
   
     return returnString
 end
+
+def decrypt(string, offset)
+  abcUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  abcLower = "abcdefghijklmnopqrstuvwxyz"
+
+  returnString = ""
+
+  string.each_char do |char|
+    #only apply shift if char is a letter
+    if abcUpper.include?(char)
+      returnString += abcUpper[(abcUpper.index(char) - offset) % 26]
+    elsif abcLower.include?(char)
+      returnString += abcLower[(abcLower.index(char) - offset) % 26]
+    else
+      returnString += char
+    end
+  
+  end
+  
+    return returnString
+end
